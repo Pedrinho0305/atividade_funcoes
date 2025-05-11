7dd0bbb390499afe679df98eef42647e87e1117b
Pedrinho0305 santos <ph0305.pereiradossantos@gmail.com>
2025-05-11 15:59:21 +0000

    Exemplo 2 - feito

--- main.js
+++ main.js
@@ -122,7 +122,7 @@ let total = () =>{
 
 total()
 */
-
+/*
 let horas = Number(prompt("Digite quantas horas foram gastas na viagem: "))
 let velocidadeMedia = Number(prompt("Digite a velocidade média: "))
 let quilometrosTotais = velocidadeMedia * horas
@@ -150,4 +150,14 @@ function somar(a, b){
  console.log(`X3 = ${soma3}`)
 
 */ 
+
+let media = (nota1, nota2) => ((nota1 * 3.5) + (nota2 * 7.5)) / 11
+
+let media1 = media(5, 7.1)
+let media2 = media(0, 7.1)
+let media3 = media(10, 10)
+
+console.log(`Média 1 = ${media1.toFixed(5)}`)
+console.log(`Média 2 = ${media2.toFixed(5)}`)
+console.log(`Média 3 = ${media3.toFixed(5)}`)
 
